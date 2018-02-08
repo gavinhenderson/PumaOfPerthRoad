9ae328dbf878e5f7272039de554cb97f31c529df
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-02-08 22:36:08 +0000

    Added video and worked on game more

---
+++ Assets/ItemGlow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGlow : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnMouseOver()
+    {
+        Debug.Log(transform.name);
+        var renderer = GetComponent<Renderer>();
+        renderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+    }
+
+    void OnMouseExit()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material.shader = Shader.Find("Diffuse");
+    }
+}
