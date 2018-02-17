79c5b5aa1fdcfa6899f05938dc85f310cd1008c2
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-02-17 12:28:29 +0000

    Stock market controller added

---
+++ Assets/StockController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Market
+{
+
+}
+
+public class Asset
+{
+
+}
+
+public class Portfolio
+{
+
+}
+
+public class StockController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
