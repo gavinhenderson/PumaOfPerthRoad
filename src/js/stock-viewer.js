2073d40895c2dac8693cde83ccdeedba85bd957f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-03-05 21:17:45 +0000

    Less DOM updates

--- src/js/stock-viewer.js
+++ src/js/stock-viewer.js
@@ -1,30 +1,40 @@
 class StockViewer{
   constructor(market){
     this.market = market;
+
+    $('#stock-viewer').empty();
+    //this.market.sortStocks();
+    this.market.iterate((stock) => {
+      var icon = "";
+      if(stock.getDiff()>0){
+        icon="green fas fa-chevron-up";
+      }else{
+        icon="red fas fa-chevron-down";
+      }
+      var html = `
+        <tr>
+          <td>`+stock.name+`</td>
+          <td id="`+stock.name+`Price">$`+stock.price.toFixed(2)+`</td>
+          <td id="`+stock.name+`Diff">`+stock.getDiff().toFixed(2)+`</td>
+          <td><i id="`+stock.name+`Icon" class="`+icon+`"></i></td>
+          <td><button onclick="buysell.select('`+stock.name+`')">Select</button></td>"
+        </tr>`
+      $('#stock-viewer').append(html);
+    })
   }
 
   update(){
-    $('#stock-viewer').empty();
-    this.market.sortStocks();
-    this.market.iterate((stock) => {
+    this.market.iterate(stock => {
       var icon = "";
       if(stock.getDiff()>0){
         icon="<i class='green fas fa-chevron-up'></i>";
       }else{
         icon="<i class='red fas fa-chevron-down'></i>";
       }
-      var html = `
-        <tr>
-          <td>`+stock.name+`</td>
-          <td>$`+stock.price.toFixed(2)+`</td>
-          <td>`+stock.getDiff().toFixed(2)+`</td>
-          <td>`+icon+`</td>
-          <td><button onclick="buysell.select('`+stock.name+`')">Select</button></td>"
-        </tr>`
-
-      //var html = "<li><p class='align-left'>"+stock.name+" $"+stock.price.toFixed(2)+" </p><p class='align-right'>"+stock.getDiff().toFixed(2)+"</p></li>";
-      //html += "<div style='clear: both;'></div>"
-      $('#stock-viewer').append(html);
+      $('#'+stock.name+'Price').text("$"+stock.price.toFixed(2));
+      $('#'+stock.name+'Diff').text(stock.getDiff().toFixed(2));
+      $('#'+stock.name+'Icon').empty();
+      $('#'+stock.name+'Icon').append(icon);
     })
   }
 }
