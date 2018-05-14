60b44d101ce5e8272a5baa8fd20e5dc5e03391e3
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-05-14 15:06:10 +0100

    small spelling fixes

--- static/view/WelcomeWindow.js
+++ static/view/WelcomeWindow.js
@@ -7,8 +7,8 @@ module.exports = class {
     $('body').append(`
       <div id="welcome-window" style="width:500px; left: 10px; top:10px;" class="window on-top">
         <h1 class="window title">Welcome to Puma of Perth Road</h1>
-        <p style="text-align: justify">Your task is to see how many days you last as an up and coming stock broker in the trading city of Dundee</p>
-        <p style="text-align: justify">If throughout the game you need any help simply click the title of a window and it will tell you what to do</p>
+        <p style="text-align: justify">Your task is to see how many days you last as an up and coming stock broker in the trading city of Dundee.</p>
+        <p style="text-align: justify">If throughout the game you need any help simply click the title of a window and it will tell you what to do.</p>
         <h3 class="window subtitle">Odd Jobs</h3>
         <p style="text-align: justify; margin-top:5px;">You can take on odd jobs to make money to make ends meet. Each job will give you a certain amount of money and will take a certain amount of time. As time goes by you will be able to do more jobs.</p>
         <h3 class="window subtitle">Calendar</h3>
